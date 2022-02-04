510916493f5e6f18cc0ec31d60e2ded921c60088
Cristian Bonilla <45046313+crbonilla619@users.noreply.github.com>
2022-02-04 12:01:33 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./components/Header.js"
+import Memes from "./components/Memes.js"
+import './styles.css'
+
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+    <div>
+        <Header />
+        <Memes />
+    </div>
+)}
